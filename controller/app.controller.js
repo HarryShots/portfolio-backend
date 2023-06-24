c4e3486a90e25d2aae5dbc92f9ab93b63ecf5c26
HarryShots <bobgalaharrison8@gmail.com>
2023-06-24 13:43:29 +0100

    updates

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,18 +1,7 @@
-const {portfolioModel} = require("../model/portfolio_model");
+
 const {testimonialsModel} = require("../model/testimonial_model");
 
-exports.portfolioAppRoute = async (req, res) => {
-    try{
-        items = await portfolioModel.find({});
-        return res.status(200).send({
-            message: "Success",
-            data: items
-        })
-    }
-    catch (error) {
-        res.status(500).json({error:"Internal Server Error"});
-    }
-}
+
 
 
 exports.testimonialsAppRoute = async (req, res) => {
--- router/app.route.js
+++ router/app.route.js
@@ -1,9 +1,9 @@
-const portfolioController = require("../controller/app.controller");
+
 const testimonialsController = require("../controller/app.controller");
 const express = require ("express");
 const router = express.Router();
 
-router.get('/portfolio', portfolioController.portfolioAppRoute);
+
 router.get('/testimonials', testimonialsController.testimonialsAppRoute);
 
 module.exports = router;
